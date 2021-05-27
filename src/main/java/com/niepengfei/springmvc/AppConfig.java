f735e82cf8e23ccc08030886b96c6556610fe8ac
pfnie@yahoo.com <pfnie@yahoo.com>
2021-05-27 14:59:56 +0800

    log

---
+++ src/main/java/com/niepengfei/springmvc/AppConfig.java
@@ -0,0 +1,9 @@
+package com.niepengfei.springmvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(value = "com.niepengfei.springmvc")
+public class AppConfig {
+}
