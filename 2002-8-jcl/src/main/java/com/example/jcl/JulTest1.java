112b209c0eb334e904fb8a6d133e4875c97eb7fd
pfnie <pfnie@yahoo.com>
2021-12-02 12:48:01 +0800

    d

---
+++ 2002-8-jcl/src/main/java/com/example/jcl/JulTest1.java
@@ -0,0 +1,13 @@
+package com.example.jcl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class JulTest1 {
+
+    static Log log = LogFactory.getLog(JulTest1.class);
+
+    public static void main(String[] args) {
+        log.info("hello world.");
+    }
+}
